16a2f3a12416a63d60e5b8468d73fd7854e09097
Tarik En-nakdi <tarik.en-nakdi@medikura.com>
2020-06-06 20:53:48 +0200

    :sparkles: add the component to fetch the data

---
+++ src/Components/componentone.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import axios from 'axios';
+
+export default class ItemList extends React.Component {
+  state = {
+    items: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://dev-new-compass.mifcom.de/testing/getconfigs`)
+      .then(res => {
+        const items = res.data;
+        this.setState({ items });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.items.map(item => 
+        <li>{item.name} : {item.description}</li>
+        )}
+      </ul>
+    )
+  }
+}
